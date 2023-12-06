014c13126dd9108840f21656957edb00b432c5aa
midrare <midrare9@gmail.com>
2023-12-05 20:22:08 -0800

    feat: add 2077.nvim color scheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -30,6 +30,7 @@ return {
   { 'fenetikm/falcon', priority = 1000, lazy = true },
   { 'lunarvim/darkplus.nvim', priority = 1000, lazy = true },
   { 'samueljoli/cyberpunk.nvim', priority = 1000, lazy = true },
+  { 'akai54/2077.nvim', priority = 1000, lazy = true },
   {
     'pineapplegiant/spaceduck',
     priority = 1000,
