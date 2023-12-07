10259c985c627f3a18ea9d3483b12252324c2f7a
midrare <midrare9@gmail.com>
2023-12-07 12:44:16 -0800

    refactor: move keybinds into init()

--- lua/plugins/tabline.lua
+++ lua/plugins/tabline.lua
@@ -1,42 +1,3 @@
-local config = require("user.config")
-config.ignored_filetypes = config.ignored_filetypes or {}
-
-config.keymaps = config.keymaps or {}
-config.keymaps.n = config.keymaps.n or {}
-
-config.keymaps.n['[b'] = {
-  cmd = function()
-    require("cokeline.mappings").by_step("focus", -1)
-  end,
-  label = 'previous buffer',
-  repeatable = true,
-}
-
-config.keymaps.n[']b'] = {
-  cmd = function()
-    require("cokeline.mappings").by_step("focus", 1)
-  end,
-  label = 'next buffer',
-  repeatable = true,
-}
-
-config.keymaps.n['[B'] = {
-  cmd = function()
-    require("cokeline.mappings").by_step("switch", -1)
-  end,
-  label = 'prev buffer pos',
-  repeatable = true,
-}
-
-config.keymaps.n[']B'] = {
-  cmd = function()
-    require("cokeline.mappings").by_step("switch", 1)
-  end,
-  label = 'next buffer pos',
-  repeatable = true,
-}
-
-
 return {
   {
     "willothy/nvim-cokeline",
@@ -46,6 +7,45 @@ return {
       "kyazdani42/nvim-web-devicons",
       "stevearc/resession.nvim",
     },
+    init = function(m)
+      local config = require("user.config")
+      config.ignored_filetypes = config.ignored_filetypes or {}
+
+      config.keymaps = config.keymaps or {}
+      config.keymaps.n = config.keymaps.n or {}
+
+      config.keymaps.n['[b'] = {
+        cmd = function()
+          require("cokeline.mappings").by_step("focus", -1)
+        end,
+        label = 'previous buffer',
+        repeatable = true,
+      }
+
+      config.keymaps.n[']b'] = {
+        cmd = function()
+          require("cokeline.mappings").by_step("focus", 1)
+        end,
+        label = 'next buffer',
+        repeatable = true,
+      }
+
+      config.keymaps.n['[B'] = {
+        cmd = function()
+          require("cokeline.mappings").by_step("switch", -1)
+        end,
+        label = 'prev buffer pos',
+        repeatable = true,
+      }
+
+      config.keymaps.n[']B'] = {
+        cmd = function()
+          require("cokeline.mappings").by_step("switch", 1)
+        end,
+        label = 'next buffer pos',
+        repeatable = true,
+      }
+    end,
     config = true,
   },
   {
