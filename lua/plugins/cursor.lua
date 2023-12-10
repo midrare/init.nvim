3048439c47ff87d0297e37029755f07e4df9cdd6
midrare <midrare9@gmail.com>
2023-12-10 10:21:19 -0800

    feat: add flit.nvim

--- lua/plugins/cursor.lua
+++ lua/plugins/cursor.lua
@@ -1,7 +1,13 @@
 return {
-  'ggandor/leap.nvim',
-  event = "VeryLazy",
-  config = function(m, opts)
-    require("leap").add_default_mappings()
-  end
+  {
+    'ggandor/leap.nvim',
+    event = "VeryLazy",
+    config = function(m, opts)
+      require("leap").add_default_mappings()
+    end
+  }, {
+    'ggandor/flit.nvim',
+    event = 'VeryLazy',
+    config = true,
+  }
 }
