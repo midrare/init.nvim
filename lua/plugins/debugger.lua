e60376b3ddeac59083173285fd23dd2254c36385
midrare <midrare9@gmail.com>
2024-03-18 17:23:04 -0700

    fix: update dependencies

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -124,7 +124,7 @@ return {
     'rcarriga/nvim-dap-ui',
     event = "VeryLazy",
     config = true,
-    dependencies = { 'mfussenegger/nvim-dap' },
+    dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
   },
   {
     'theHamsta/nvim-dap-virtual-text',
