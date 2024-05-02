406104ca1810663352f4933589944f58f10225ec
midrare <midrare9@gmail.com>
2024-05-02 13:26:36 -0700

    feat: add nvim-notify

--- lua/plugins/vimui.lua
+++ lua/plugins/vimui.lua
@@ -1,7 +1,22 @@
+local config = require("user.config")
+config.telescope = config.telescope or {}
+config.telescope.extensions = config.telescope.extensions or {}
+table.insert(config.telescope.extensions, 'notify')
+
 return {
   {
     'stevearc/dressing.nvim',
-    event = "VeryLazy",
+    event = 'VeryLazy',
     config = true,
-  }
+  },
+  {
+    'rcarriga/nvim-notify',
+    event = "VimEnter",
+    init = function()
+      vim.notify = require("notify")
+    end,
+    opts = {
+      render = "wrapped-compact",
+    },
+  },
 }
