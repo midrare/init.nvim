740a7011a1365f266e4c33f5adb29809d11f6388
midrare <midrare9@gmail.com>
2024-06-18 12:04:45 -0700

    refactor: switch to asterisk.vim

--- lua/plugins/asterisk.lua
+++ lua/plugins/asterisk.lua
@@ -5,25 +5,24 @@ config.keymaps.n = config.keymaps.n or {}
 config.keymaps.x = config.keymaps.x or {}
 
 config.keymaps.n['*'] = {
-  cmd = function()
-    require('lasterisk').search()
-  end,
+  cmd = '<Plug>(asterisk-z*)',
   label = 'word',
 }
-config.keymaps.n['g*'] = {
-  cmd = function()
-    require('lasterisk').search({ is_whole = false })
-  end,
+config.keymaps.n['#'] = {
+  cmd = '<Plug>(asterisk-z#)',
   label = 'partial word',
 }
 config.keymaps.x['g*'] = {
-  cmd = function()
-    require('lasterisk').search()
-  end,
-  label = 'word',
+  cmd = '<Plug>(asterisk-gz*)',
+  label = 'word (backwards)',
+}
+
+config.keymaps.x['g#'] = {
+  cmd = 'map g# <Plug>(asterisk-gz#)',
+  label = 'partial word (backwards)',
 }
 
 return {
-  'rapan931/lasterisk.nvim',
-  lazy = true,
+  'haya14busa/vim-asterisk',
+  event = "VeryLazy",
 }
