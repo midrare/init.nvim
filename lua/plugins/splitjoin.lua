b9d5c1c48249fa5d27823b26d3e8c23c166cc882
midrare <midrare9@gmail.com>
2024-07-05 14:23:17 -0700

    refactor: toggle split-join

--- lua/plugins/splitjoin.lua
+++ lua/plugins/splitjoin.lua
@@ -4,17 +4,10 @@ config.keymaps = config.keymaps or {}
 config.keymaps.n = config.keymaps.n or {}
 config.keymaps.x = config.keymaps.x or {}
 
-config.keymaps.n['gj'] = {
-  label = 'join object',
-  cmd = function()
-    require('treesj').join()
-  end,
-}
-
 config.keymaps.n['g,'] = {
-  label = 'split object',
+  label = 'split/join object',
   cmd = function()
-    require('treesj').split()
+    require('treesj').toggle()
   end,
 }
 
