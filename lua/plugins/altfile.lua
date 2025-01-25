bb16fdf48ecac44aad3cb974d25381e01e6dd226
midrare <midrare9@gmail.com>
2025-01-25 15:05:25 -0800

    feat: jump to alt file for cpp

--- lua/plugins/altfile.lua
+++ lua/plugins/altfile.lua
@@ -5,6 +5,11 @@ return {
     require('other-nvim').setup({
       mappings = {
         'c',
+        {
+          pattern = "(.*)%.([ch])(pp)?$",
+          target = "/tests/%1_test.c%3",
+          context = "test",
+        }
       },
     })
   end,
