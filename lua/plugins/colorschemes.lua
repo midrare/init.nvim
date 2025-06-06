cf077c5179c0b591bc3f4a77fa2f4cae699b31f0
midrare <midrare9@gmail.com>
2025-06-05 23:43:40 -0700

    refactor: add citruszest color scheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -30,6 +30,7 @@ return {
   { 'akai54/2077.nvim', priority = 1000, lazy = true },
   { 'LunarVim/horizon.nvim', priority = 1000, lazy = true },
   { 'vague2k/vague.nvim', priority = 1000, lazy = true },
+  { 'zootedb0t/citruszest.nvim', priority = 1000, lazy = true },
   {
     'pineapplegiant/spaceduck',
     priority = 1000,
